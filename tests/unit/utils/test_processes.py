30b121c6daeb07211631a9370a18792950367fd3
Jan Müller <jan-mue@users.noreply.github.com>
2025-08-09 15:20:07 +0200

    fix

--- tests/unit/utils/test_processes.py
+++ tests/unit/utils/test_processes.py
@@ -13,7 +13,7 @@ if TYPE_CHECKING:
 
 def test_terminate_or_kill_process_on_port(mocker: MockerFixture) -> None:
     mock_proc = mocker.Mock()
-    mocker.patch("pycodium.main.get_process_on_port", return_value=mock_proc)
+    mocker.patch("pycodium.utils.processes.get_process_on_port", return_value=mock_proc)
     mock_proc.wait.return_value = None
 
     terminate_or_kill_process_on_port(9999, timeout=1)
@@ -23,8 +23,8 @@ def test_terminate_or_kill_process_on_port(mocker: MockerFixture) -> None:
 
 
 def test_terminate_or_kill_process_on_port_no_proc(mocker: MockerFixture) -> None:
-    mock_logger = mocker.patch("pycodium.main.logger")
-    mocker.patch("pycodium.main.get_process_on_port", return_value=None)
+    mock_logger = mocker.patch("pycodium.utils.processes.logger")
+    mocker.patch("pycodium.utils.processes.get_process_on_port", return_value=None)
 
     terminate_or_kill_process_on_port(8888, timeout=1)
 
@@ -32,13 +32,13 @@ def test_terminate_or_kill_process_on_port_no_proc(mocker: MockerFixture) -> Non
 
 
 def test_wait_for_port_success(mocker: MockerFixture) -> None:
-    mock_get_process_on_port = mocker.patch("pycodium.main.get_process_on_port")
+    mock_get_process_on_port = mocker.patch("pycodium.utils.processes.get_process_on_port")
     wait_for_port(12345, timeout=1)
     mock_get_process_on_port.assert_called_with(12345)
 
 
 def test_wait_for_port_timeout(mocker: MockerFixture) -> None:
-    mocker.patch("pycodium.main.get_process_on_port", return_value=None)
+    mocker.patch("pycodium.utils.processes.get_process_on_port", return_value=None)
     start = time.time()
     with pytest.raises(TimeoutError):
         wait_for_port(54321, timeout=1)

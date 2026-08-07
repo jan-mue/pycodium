e89b807971b721e4813a5f719e5320096afcb31a
Jan Müller <jan-mue@users.noreply.github.com>
2026-08-07 15:25:06 +0200

    fix: Downgrade Reflex to version 0.9.5.post2 (#751)

--- pycodium/components/file_explorer.py
+++ pycodium/components/file_explorer.py
@@ -63,9 +63,13 @@ class FileTree(rx.ComponentState):
 
 file_tree_view = FileTree.create
 
+EMPTY_PATH: rx.Var[str] = rx.Var.create("")
+
 
 @rx.memo
-def file_tree(name: str, sub_paths: list[FilePath], is_dir: bool, path: str = "") -> rx.Component:
+def file_tree(
+    name: rx.Var[str], sub_paths: rx.Var[list[FilePath]], is_dir: rx.Var[bool], path: rx.Var[str] = EMPTY_PATH
+) -> rx.Component:
     """Render a file tree item."""
     return file_tree_view(name=name, sub_paths=sub_paths, is_dir=is_dir, path=path)
 
